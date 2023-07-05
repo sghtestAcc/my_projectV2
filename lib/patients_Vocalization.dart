8162d00a84e59e5526dd0f35fdbae2bde1980ee0
Unknownstranger-debug <briantan438@gmail.com>
2023-07-06 04:09:18 +0800

    Got speech to text function working

--- lib/patients_Vocalization.dart
+++ lib/patients_Vocalization.dart
@@ -1,6 +1,8 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_mlkit_translation/google_mlkit_translation.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class PatientsVocalScreen extends StatefulWidget {
   const PatientsVocalScreen({super.key});
@@ -10,7 +12,7 @@ class PatientsVocalScreen extends StatefulWidget {
 }
 
 class _PatientsVocalScreenState extends State<PatientsVocalScreen> {
-   String? _translatedText;
+  String? _translatedText;
   final _controller = TextEditingController();
   String sourceSelect = 'English';
   String targetSelect = 'English';
@@ -49,166 +51,214 @@ class _PatientsVocalScreenState extends State<PatientsVocalScreen> {
       .toList();
   // PatientHomeScreen
 
-Widget buildCard(int index) => Container(
-  padding: EdgeInsets.all(10.0),
-  width: 100,
-  decoration: BoxDecoration(
-    borderRadius: BorderRadius.all(Radius.circular(22)),
-    color: Color(0xFFF6F6F6),
-    boxShadow: [
-      BoxShadow(
-        color: Color.fromRGBO(0, 0, 0, 0.2),
-        offset: Offset(0, 1),
-        blurRadius: 4,
-        spreadRadius: 0,
-      ),
-    ],
-    border: Border.all(
-      color: Colors.black, // Set your desired border color here
-      width: 1, // Set the border width
-    ),
-  ),
-  child: Column(
-    // crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-      Text('Medication'),
-      Text('$index'),
-      SizedBox(height: 10),
-      GestureDetector(
-            onTap: () {},
-        child: Image.asset(
-          'assets/images/mic.png',
-          height: 28, width: 28, fit: BoxFit.cover
+  Widget buildCard(int index) => Container(
+        padding: EdgeInsets.all(10.0),
+        width: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(22)),
+          color: Color(0xFFF6F6F6),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromRGBO(0, 0, 0, 0.2),
+              offset: Offset(0, 1),
+              blurRadius: 4,
+              spreadRadius: 0,
+            ),
+          ],
+          border: Border.all(
+            color: Colors.black, // Set your desired border color here
+            width: 1, // Set the border width
+          ),
         ),
-      ),
-    ],
-  ),
-);
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Medication'),
+            Text('$index'),
+            SizedBox(height: 10),
+            GestureDetector(
+              onTap: () {},
+              child: Image.asset('assets/images/mic.png',
+                  height: 28, width: 28, fit: BoxFit.cover),
+            ),
+          ],
+        ),
+      );
 
+  SpeechToText speechToText = SpeechToText();
 
-   @override
+  var isListening = false;
+  var text = 'Enter Text';
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+          endRadius: 75.0,
+          animate: isListening,
+          duration: Duration(milliseconds: 2000),
+          glowColor: Color(0xff00A67E),
+          repeat: true,
+          repeatPauseDuration: Duration(milliseconds: 100),
+          showTwoGlows: true,
+          child: GestureDetector(
+            onTapDown: (details) async {
+              if (!isListening) {
+                var available = await speechToText.initialize();
+                if (available) {
+                  setState(() {
+                    isListening = true;
+                    speechToText.listen(
+                      onResult: (result) {
+                        setState(() {
+                          text = result.recognizedWords;
+                        });
+                      },
+                    );
+                  });
+                }
+              }
+            },
+            onTapUp: (details) {
+              setState(() {
+                isListening = false;
+              });
+              speechToText.stop();
+            },
+            child: CircleAvatar(
+              backgroundColor: Color(0xff00A67E),
+              radius: 35,
+              child: Icon(isListening ? Icons.mic : Icons.mic_none,
+                  color: Colors.white),
+            ),
+          )),
       appBar: AppBar(
         title: Text('Vocalization'),
-      centerTitle: true,
+        centerTitle: true,
       ),
       body: Container(
-        child: Column(
-          children: [
-           Column(
-            children: [
-               Container(
-            color: const Color(0xFF9EE8BF),
-            width: double.infinity,
-            padding: const EdgeInsets.fromLTRB(20, 20, 0, 20),
-            child: const Text(
-              'Translate Medication',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Column(children: [
+          Column(
             children: [
               Container(
-                padding: const EdgeInsets.all(10.0),
-                child: DropdownButton(
-                  hint: const Text('English'),
-                  value: sourceSelect,
-                  onChanged: (newValue) {
-                    setState(() {
-                      sourceSelect = newValue ?? '';
-                      translateTextFunction(typedText);
-                    });
-                  },
-                  items: languagePicker.map((valueItem) {
-                    return DropdownMenuItem(
-                      value: valueItem,
-                      child: Text(valueItem),
-                    );
-                  }).toList(),
+                color: const Color(0xFF9EE8BF),
+                width: double.infinity,
+                padding: const EdgeInsets.fromLTRB(20, 20, 0, 20),
+                child: const Text(
+                  'Translate Medication',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    child: DropdownButton(
+                      hint: const Text('English'),
+                      value: sourceSelect,
+                      onChanged: (newValue) {
+                        setState(() {
+                          sourceSelect = newValue ?? '';
+                          translateTextFunction(typedText);
+                        });
+                      },
+                      items: languagePicker.map((valueItem) {
+                        return DropdownMenuItem(
+                          value: valueItem,
+                          child: Text(valueItem),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    child: DropdownButton(
+                      hint: const Text('English'),
+                      value: targetSelect,
+                      onChanged: (newValue) {
+                        setState(() {
+                          targetSelect = newValue ?? '';
+                          translateTextFunction(typedText);
+                        });
+                      },
+                      items: languagePicker.map((valueItem) {
+                        return DropdownMenuItem(
+                          value: valueItem,
+                          child: Text(valueItem),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                child: TextFormField(
+                  controller: _controller,
+                  onChanged: translateTextFunction,
+                  maxLines: 4,
+                  keyboardType: TextInputType.multiline,
+                  decoration: InputDecoration(
+                    hintText: text,
+                    suffixIcon: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // InkWell(
+                        //   onTap: () {
+                        //     Clipboard.getData('text/plain').then((value) {
+                        //       if (value != null) {
+                        //         _controller.text = value.text!;
+                        //         setState(() {
+                        //           typedText = value.text!;
+                        //           translateTextFunction(typedText);
+                        //         });
+                        //       }
+                        //     });
+                        //   },
+                        //   child: Icon(Icons.paste),
+                        // ),
+                      ],
+                    ),
+                    contentPadding: EdgeInsets.all(10.0),
+                  ),
                 ),
               ),
               Container(
-                padding: const EdgeInsets.all(10.0),
-                child: DropdownButton(
-                  hint: const Text('English'),
-                  value: targetSelect,
-                  onChanged: (newValue) {
-                    setState(() {
-                      targetSelect = newValue ?? '';
-                      translateTextFunction(typedText);
-                    });
-                  },
-                  items: languagePicker.map((valueItem) {
-                    return DropdownMenuItem(
-                      value: valueItem,
-                      child: Text(valueItem),
-                    );
-                  }).toList(),
+                width: double.infinity,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                  ),
                 ),
+                child: Text(
+                  _isLoading
+                      ? 'Loading...'
+                      : _translatedText ?? 'Enter text to be translated',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                height: 130,
+                child: Text(
+                  text,
+                  style:TextStyle(fontSize: 24, color: isListening ? Colors.black87 : Colors.black54, fontWeight: FontWeight.w600),
+                ),
+                // ListView.separated(
+                //   scrollDirection: Axis.horizontal,
+                //   itemCount: 6,
+                //   separatorBuilder: (context, _) => SizedBox(width: 10,),
+                //   itemBuilder: (context, index) => buildCard(index) ,
+                // )
               ),
             ],
-          ),
-            Container(
-  child: TextFormField(
-    controller: _controller,
-    onChanged: translateTextFunction,
-    maxLines: 4,
-    keyboardType: TextInputType.multiline,
-    decoration: InputDecoration(
-      hintText: 'Enter text',
-      suffixIcon: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // InkWell(
-          //   onTap: () {
-          //     Clipboard.getData('text/plain').then((value) {
-          //       if (value != null) {
-          //         _controller.text = value.text!;
-          //         setState(() {
-          //           typedText = value.text!;
-          //           translateTextFunction(typedText);
-          //         });
-          //       }
-          //     });
-          //   },
-          //   child: Icon(Icons.paste),
-          // ),
-        ],
-      ),
-      contentPadding: EdgeInsets.all(10.0),
-    ),
-  ),
-),
-Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 2,
-              ),
-            ),
-            child: Text(
-              _isLoading
-                  ? 'Loading...'
-                  : _translatedText ?? 'Enter text to be translated',
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-            ),
-),
-SizedBox(height: 10,),
-Container(
-  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-  height: 130,
-  child:   ListView.separated(
-    scrollDirection: Axis.horizontal,
-    itemCount: 6,
-    separatorBuilder: (context, _) => SizedBox(width: 10,),
-    itemBuilder: (context, index) => buildCard(index) ,
-  ),
-),          
-              ],)
-          ]),
+          )
+        ]),
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import cloud_firestore
 import file_selector_macos
 import firebase_auth
 import firebase_core
+import speech_to_text_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
 }
