53bd63d1307a00fee2a6ccc9c5a6844e4228bd47
keane26mar <keane26mar@gmail.com>
2023-07-12 23:20:11 +0800

    bug fix for patient home screen

--- lib/screens/home/patient_home.dart
+++ lib/screens/home/patient_home.dart
@@ -1,8 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_project/components/navigation_drawer.dart';
 import 'package:my_project/models/grace_user.dart';
+import 'package:my_project/models/medications.dart';
+import 'package:my_project/repos/authentication_repository.dart';
 import 'package:my_project/screens/communications/caregiver/caregiver_prescription.dart';
 import 'package:my_project/screens/communications/caregiver/caregiver_vocal.dart';
 import 'package:my_project/models/login_type.dart';
@@ -20,7 +21,9 @@ class PatientHomeScreen extends StatefulWidget {
   State<PatientHomeScreen> createState() => _PatientHomeScreenState();
 }
 
+  final _authRepo = Get.put(AuthenticationRepository());
 class _PatientHomeScreenState extends State<PatientHomeScreen> {
+  final currentEmail = _authRepo.firebaseUser.value?.email;
   String lol = '';
   TextEditingController searchController = TextEditingController();
   final controller = Get.put(SelectPatientController());
@@ -360,21 +363,123 @@ class _PatientHomeScreenState extends State<PatientHomeScreen> {
                 ]),
           ),
           widget.loginType == LoginType.patient
-              ? Expanded(
-                  child: ListView.separated(
-                    padding: const EdgeInsets.all(10.0),
-                    itemCount: 10,
-                    separatorBuilder: (context, index) {
-                      return const SizedBox(
-                        height: 10,
-                      );
-                    },
-                    itemBuilder: (context, index) {
-                      return buildCard(index);
-                      // return patientcard(index);
-                    },
-                  ),
-                )
+              ? 
+              // Expanded(
+              //     child: ListView.separated(
+              //       padding: const EdgeInsets.all(10.0),
+              //       itemCount: 10,
+              //       separatorBuilder: (context, index) {
+              //         return const SizedBox(
+              //           height: 10,
+              //         );
+              //       },
+              //       itemBuilder: (context, index) {
+              //         return buildCard(index);
+              //         // return patientcard(index);
+              //       },
+              //     ),
+              //   )
+              Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color:const Color(0xFFF6F6F6), // Background color
+                borderRadius: BorderRadius.circular(10.0), // Rounded border
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1.0,
+                ),
+              ),
+              child:  Column(children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'PatientName 1',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    Text(
+                      'Quantity',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    Text(
+                      'Schedule',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Row(
+                  children: [
+                    Text(
+                      'xxx@gmail.com',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                FutureBuilder<List<Medication>>(
+                  future: userRepo.getPatientMedications(currentEmail!),
+                  builder: (context, snapshot) {
+                     if(snapshot.connectionState == ConnectionState.done) {
+                       if(snapshot.hasData) {
+                          var patientsInfoMedication = snapshot.data;
+                          final children = <Widget>[];
+                          for (var i = 0; i < patientsInfoMedication!.length; i++) {
+                             children.add(
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                              Container(
+                              decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12), // Adjust the value as needed for the desired roundness
+                              border: Border.all(color: Colors.black, width: 1), // Set the border color and width
+                            ),
+                              child: ClipRRect(
+                              borderRadius: BorderRadius.circular(12), // Same value as the BoxDecoration for rounded corners
+                              child: Image.network(
+                              patientsInfoMedication[i].pills ,
+                              height: 50,
+                              width: 50,
+                              fit: BoxFit.cover,
+                            ),
+                                             ),
+                                            ),
+                                Text(patientsInfoMedication[i].labels, 
+                                style: TextStyle(fontSize: 15),
+                                ),
+                                Text(patientsInfoMedication[i].quantity, 
+                                style: TextStyle(fontSize: 15),
+                                ),
+                                Text(patientsInfoMedication[i].schedule, 
+                                style: TextStyle(fontSize: 15),
+                                ),
+                                SizedBox(height: 10,),
+                             ]),
+                            //  SizedBox(height: 10,),
+                             );
+                             children.add(SizedBox(height: 10,));
+                          }
+                               return Column(
+                                children: children,);
+                       } else if (snapshot.hasError) {
+                        return Center(child: Text(snapshot.error.toString()));
+              } else {
+                 return const Center(
+                            child: Text('Something went wrong'));
+              }
+                     }  else {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  }),
+              ]),
+            ),
+          )
               : FutureBuilder<List<GraceUser>>(
                   future: controller.getPatients(),
                   builder: (context, snapshot) {
